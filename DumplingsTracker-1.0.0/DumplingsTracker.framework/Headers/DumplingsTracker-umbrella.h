550aef295326d2bc65ae5ac6bb75366135558c15
lee <lijianwei@chicv.com>
2017-03-17 12:02:34 +0800

    u

---
+++ DumplingsTracker-1.0.0/DumplingsTracker.framework/Headers/DumplingsTracker-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "DumplingsTracker.h"
+
+FOUNDATION_EXPORT double DumplingsTrackerVersionNumber;
+FOUNDATION_EXPORT const unsigned char DumplingsTrackerVersionString[];
+
