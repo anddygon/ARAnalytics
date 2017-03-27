37a1ff245d4e191c5de87df44eaa8f92a3eaccc6
lee <lijianwei@chicv.com>
2017-03-27 17:48:17 +0800

    a DumplingsTracker|FacebookProvider

---
+++ Providers/FacebookProvider.m
@@ -0,0 +1,67 @@
+#import "FacebookProvider.h"
+#import "ARAnalyticsProviders.h"
+#import "FBSDKCoreKit.h"
+
+@interface FacebookProvider ()
+
+@end
+
+@implementation FacebookProvider
+#ifdef AR_FACEBOOK_EXISTS
+
+- (instancetype)initWithIdentifier:(NSString *)identifier {
+    NSAssert([FBSDKAppEvents class], @"FBSDKCoreKit Analytics SDK is not included");
+
+    if ((self = [super init])) {
+        [FBSDKAppEvents setLoggingOverrideAppID:identifier];
+    }
+#if !DEBUG
+    [FBSDKAppEvents activateApp];
+#endif
+    return self;
+}
+
+-(void)identifyUserWithID:(NSString *)userID andEmailAddress:(NSString *)email {
+    if (userID) {
+        [FBSDKAppEvents setUserID:userID];
+    }
+}
+
+- (void)event:(NSString *)event withProperties:(NSDictionary *)properties {
+#if DEBUG
+    return;
+#endif
+    NSMutableDictionary *props = [[NSMutableDictionary alloc] initWithDictionary:properties];
+    if ([self.eventMappings objectForKey:event]) {
+        event = [self.eventMappings objectForKey:event];
+    }
+    for(NSString *key in properties.allKeys) {
+        NSString *nk = [self.customDimensionMappings objectForKey:key];
+        if(nk) {
+            [props setObject:[properties objectForKey:key] forKey:nk];
+            [props removeObjectForKey:key];
+        }
+    }
+    NSString * price = props[@"price"];
+    if ([event isEqualToString:@"purchase"]) {
+        NSString *currency = props[FBSDKAppEventParameterNameCurrency];
+        [FBSDKAppEvents logPurchase:price.doubleValue currency:currency parameters:props];
+        return;
+    }
+    if (!price) {
+        [FBSDKAppEvents logEvent:event parameters:props];
+    }else{
+        [FBSDKAppEvents logEvent:event valueToSum:price.doubleValue parameters:props];
+    }
+}
+
+- (void)didShowNewPageView:(NSString *)pageTitle {
+
+}
+
+- (void)didShowNewPageView:(NSString *)pageTitle withProperties:(NSDictionary *)properties {
+
+}
+
+#endif
+@end
